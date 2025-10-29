dc82de8af40d3f6662d0ed931c8a06795390235e
Ravinder bhatoy <ravinderd.bhatoy@gmail.com>
2025-10-29 08:05:54 +0530

    new sections and remove old ones

--- public/content/content.js
+++ public/content/content.js
@@ -1,22 +1,33 @@
 const Content = {
-    intro: "Hi, I'm Ravinder (Bindu)! I'm a passionate Python developer with a love for both front-end and back-end development.<br> My toolkit includes Python, Flask, Django, React.js, HTML, and CSS. <br>You'll often find me exploring the command line in my favorite Linux environment.<br> I'm driven by a constant curiosity to learn new technologies and explore the world of creative coding.",
-
-    hobbies: `🎮 Gaming: I enjoy playing video games, some of my favorites being Witcher 3, RDR2, and GTA Vice City.<br>
+    // intro: "Hi, I'm Ravinder ! I'm a passionate Python developer with a love for both front-end and back-end development.<br> My toolkit includes Python, Flask, Django, React.js, HTML, and CSS. <br>You'll often find me exploring the command line in my favorite Linux environment.<br> I'm driven by a constant curiosity to learn new technologies and explore the world of creative coding.",
+    //
+    hobbies: `
+👨‍👩‍👧‍👦 Socializing: I love spending time with family and friends. It's a source of great joy and comfort for me.<br>
+🎮 Gaming: I enjoy playing video games, some of my favorites being Witcher 3, RDR2, Elden Ring and GTA Vice City.<br>
 🏏 Cricket: I like playing cricket with friends, even though I don't get to play as often as I'd like.<br>
 📺 Anime: I enjoy watching. While I haven't seen a huge number of series, it's something I find enjoyable.<br>
-👨‍👩‍👧‍👦 Socializing: I love spending time with family and friends. It's a source of great joy and comfort for me.<br>
 🎵 Music: I enjoy listening to music, especially older Sufi,Ghazals and Qawwali music.<br>
 😌 Relaxing: I appreciate quiet time by myself, often with a cup of tea.<br>
 📖 Reading: I've recently started reading books to build a habit, and I've found that I really enjoy it.<br>
 💻 Tech and Coding: I also enjoy reading blogs and articles related to technology and coding.`,
 
+  dailyDrivers: `
+  🐧 System Environment — Linux (EndeavourOS) with i3wm.<br>
+  💻 Code Editor — Neovim (NvChad).<br>
+  🗂️ File Manager — Spf <a className="p-7" href="https://superfile.dev/" target="_blank"> (Super File Manager) </a>.<br>
+  🌐 Web Browser — Zen Browser.<br>
+  📝 Productivity — Notion.<br>
+  🎨 Design Tool — Figma.<br>
+  `,
+
     about: "This section shows the coding projects I've worked on. You'll find explanations, pictures, and sometimes examples of how they work. All of my projects are available on my Github.",
     current: `
-👨‍💻 Working as a full time software Developer.<br>
-{} Learing DSA from Striver DSA Cheat Sheet. <br>
-🏛️ Final year of Bachelor's degree(BCA).<br>
-☯  Rewatching Hunter X Hunter (Anime).<br>
-🇬 Learning Go (golang) out of curiosity.<br>
+
+🏛️ Pursuing Master's in Computer Application (MCA).<br>
+{} Exploring Datascience and Machine Learning. <br>
+👨‍💻 Twikling around with linux.<br>
+⚔️ On my quest to become the Elden Lord (Elden Ring). <br>
+🌍 Making memories with wonderful people along the way.
 `,
     projects: [
         {title: "DevSearch(Django)", id: "devsearch"},
@@ -25,14 +36,8 @@ const Content = {
         {title: "E-Commerce Store(Django)", id: "ecommerce-django"},
         {title: "Frontend-Mentor", id: "frontend-mentor"},
         {title: "Dashboard(TailwindCSS)", id: "dashboard-tailwindcss"},
-    ]
+    ],
 }
 
 export default Content;
-
-
-
-
-
-
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,33 +7,33 @@ import Content from '../../public/content/content'
 function Home() {
   return (
     <>
-      <div className="container mx-auto md:px-12 py-12 space-y-12 h-[calc(100vh-3rem)]">
-        <Navbar />
-        <div className="grid gap-8 lg:grid-cols-4 h-full lg:overflow-y-auto no-scrollbar scroll-smooth">
-            <ProfileBox />
-          <div className="lg:col-span-3 h-fit space-y-8">
-            <Postcard
-              heading={"Who am I"}
-              description={Content.intro}
-              paraText={true}
-              icon="/logo/intro-emote.svg"
-            />
-            <Postcard
-              paraText={true}
-              heading={"My Hobbies"}
-              description={Content.hobbies}
-              icon="/logo/football.svg"
-            />
-            <Postcard
-              paraText={true}
-              date={'Monday, March 10, 2025'}
-              heading={"What I am doing currently"}
-              description={Content.current}
-              icon="/logo/football.svg"
-            />
-          </div>
-        </div>
-      </div>
+    <div className="container mx-auto md:px-12 py-12 space-y-12 h-[calc(100vh-3rem)]">
+    <Navbar />
+    <div className="grid gap-8 lg:grid-cols-4 h-full lg:overflow-y-auto no-scrollbar scroll-smooth">
+    <ProfileBox />
+    <div className="lg:col-span-3 h-fit space-y-8">
+      <Postcard
+      paraText={true}
+      date={'October 27, 2025'}
+      heading={"What I am doing currently"}
+      description={Content.current}
+      icon="/logo/football.svg"
+      />
+      <Postcard
+      paraText={true}
+      heading={"My Hobbies"}
+      description={Content.hobbies}
+      icon="/logo/football.svg"
+      />
+    <Postcard
+      paraText={true}
+      heading={"My Daily Drivers"}
+      description={Content.dailyDrivers}
+      icon="/logo/football.svg"
+      />
+    </div>
+    </div>
+    </div>
     </>
   )
 }
